6b7d552dd4dbe0a4ced247640384c7ed769928cb
anatoly burtsev <anatolyburtsev@yandex.ru>
2015-11-12 10:41:51 +0300

    fix count of posts in queue

--- logic.py
+++ logic.py
@@ -4,6 +4,7 @@ import vk_api
 import config
 import sql_schedule_api
 import logging
+import time
 logging.basicConfig(level=logging.DEBUG)
 
 
@@ -16,14 +17,15 @@ def how_many_posts_in_queue(vk_group):
     schedule = sql_schedule_api.get_all_tags_and_time_in_order()
     for tag, hour in schedule:
         is_exist_post_in_queue = True
-        post_in_queue = 1
+        post_in_queue = 0
         while is_exist_post_in_queue:
             time_for_post_in_future = int(convert_hours_and_day_to_epoch(hour, post_in_queue))
-            if time_for_post_in_future not in postponed_posts_time:
+            if time_for_post_in_future not in postponed_posts_time and time_for_post_in_future > time.time():
                 is_exist_post_in_queue = False
-            post_in_queue += 1
+            else:
+                post_in_queue += 1
 
-        result.append((tag, hour, post_in_queue-1))
+        result.append((tag, hour, post_in_queue))
     return result
 
 
